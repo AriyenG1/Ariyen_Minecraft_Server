2d5d2533435e5fe0670fac11772ab74bbea1c02c
AriyenG1 <76100892+AriyenG1@users.noreply.github.com>
2021-05-12 23:35:37 +0200

    Repost

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-document.addEventListener(
-  "scroll",
-  () => {
+function handleScroll() {
     navbar = document.querySelector("nav");
 
-    if (window.scrollY > navbar.offsetTop) {
-      navbar.classList.add("scrolled_by");
+    if (window.scrollY > 0) {
+        navbar.classList.add("scrolled_by");
     } else {
-      navbar.classList.remove("scrolled_by");
+        navbar.classList.remove("scrolled_by");
     }
-  },
-  {
-    passive: true,
-  }
-);
+}
 
-document.querySelector("#clipboard").addEventListener("click", async () => {
-  var textToCopy = document.querySelector("#adress");
-  await navigator.clipboard.writeText(textToCopy.textContent);
-  alert("Play.alve.dev has been copied to your clipboard");
-});
+async function handleClipboardClick() {
+    var textToCopy = document.querySelector("#adress").textContent;
+    await navigator.clipboard.writeText(textToCopy);
+    alert("play.alve.dev has been copied to your clipboard");
+}
+
+document.addEventListener("scroll", handleScroll, {passive: true});
+document.querySelector("#clipboard").addEventListener("click", handleClipboardClick);
